ec5f8084e9b83e04c598c49602f9b53b071c3716
samliweisen <liweisen007@gmail.com>
2018-01-31 19:42:20 +0000

    add projects component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 
 import Experiences from './Experiences.jsx';
 import Skills from './Skills.jsx';
+import Projects from './Projects.jsx';
 import Clock from './Clock.jsx';
 import Weather from './Weather.jsx';
-import {Box, BoxTitle} from './style.jsx';
-import {userInfo, projects} from './state.js';
+import {Box} from './style.jsx';
+import {userInfo} from './state.js';
 
 const Container = styled.div`
     max-width: 1200px;
@@ -14,16 +15,6 @@ const Container = styled.div`
     margin: auto;
     display: flex;
     flex-wrap: wrap;
-`;
-
-const Link = styled.a`
-    color: #000000;
-    text-decoration: none;
-    transition: 0.3s ease-in;
-    &:hover {
-        text-decoration: underline;
-        color: #06A763;
-    }
 `;
 const Intro = styled.div`
     padding: 20px;
@@ -33,25 +24,6 @@ const Intro = styled.div`
 
 export default class App extends React.Component {
     render() {
-        const pros = projects.map((p, i) => {
-            const steps = p.steps.map((s, j) => {
-                return (
-                    <li key={j}>
-                        {s}
-                    </li>
-                );
-            });
-            return (
-                <div className="project" key={i}>
-                    <h3 className="project__title">
-                        <Link target="_blank" rel="noopener" href={p.link}><i className="fa fa-link" aria-hidden="true"></i>{p.name}</Link> - <span className="project__type">{p.type}</span></h3>
-                    <ul>
-                        {steps}
-                    </ul>
-                </div>
-            );
-        });
-        
         return (
             <Container>
                 <main id="main">
@@ -62,15 +34,7 @@ export default class App extends React.Component {
                         </Intro>
                     </Box>
                     <Experiences />
-                    <Box className="projects">
-                        <BoxTitle>
-                            <i className="fa fa-tasks" aria-hidden="true"></i>
-                            <span>Projects</span>
-                        </BoxTitle>
-                        <div className="box__body">
-                        {pros}
-                        </div>
-                    </Box>
+                    <Projects />
                 </main>
                 <aside id="aside">
                     <Weather />
---
+++ client/components/Projects.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {projects} from './state.js';
+import {Box, BoxTitle} from './style.jsx';
+
+const Link = styled.a`
+    color: #000000;
+    text-decoration: none;
+    transition: 0.3s ease-in;
+    &:hover {
+        text-decoration: underline;
+        color: #06A763;
+    }
+`;
+
+export default class Projects extends React.Component {
+    render() {
+        const pros = projects.map((p, i) => {
+            const steps = p.steps.map((s, j) => {
+                return (
+                    <li key={j}>
+                        {s}
+                    </li>
+                );
+            });
+            return (
+                <div className="project" key={i}>
+                    <h3 className="project__title">
+                        <Link target="_blank" rel="noopener" href={p.link}><i className="fa fa-link" aria-hidden="true"></i>{p.name}</Link> - <span className="project__type">{p.type}</span></h3>
+                    <ul>
+                        {steps}
+                    </ul>
+                </div>
+            );
+        });
+        return(
+            <Box className="projects">
+                <BoxTitle>
+                    <i className="fa fa-tasks" aria-hidden="true"></i>
+                    <span>Projects</span>
+                </BoxTitle>
+                <div className="box__body">
+                {pros}
+                </div>
+            </Box>
+        );
+    }
+}
