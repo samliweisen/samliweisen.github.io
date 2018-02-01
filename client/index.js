d320dab48a8ef7b73a957a732ef78d2aca0fe58a
samliweisen <liweisen007@gmail.com>
2018-02-01 21:50:12 +0000

    add react redux

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+
 import App from './components/App.jsx';
 
 render(<App />, document.getElementById('root'));
