bb3b0647981ccd756bb2857dde7baff2a35c626c
samliweisen <liweisen007@gmail.com>
2018-02-04 22:23:25 +0000

    add cache pollify and service working to cache file

---
+++ sw.js
@@ -0,0 +1,29 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+
+    e.waitUntil(
+    
+        caches.open('samresume').then(function(cache) {
+        
+            return cache.addAll([
+            
+                '/',
+                
+                '/index.html',
+                
+                '/index.html?homescreen=1',
+                
+                '/?homescreen=1',
+                
+                '/style.css',
+                
+                '/index_bundlejs',
+            
+            ]);
+        
+        })
+    
+    );
+
+});
