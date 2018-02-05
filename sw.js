42685f76fa020c8bd0dbe2efbc7b5214c4c84759
samliweisen <liweisen007@gmail.com>
2018-02-05 01:58:37 +0000

    update service worker

--- sw.js
+++ sw.js
@@ -1,23 +1,49 @@
-self.addEventListener('install', function(e) {
-
-    e.waitUntil(
+(function() {
+    'use strict';
+    var staticFilesToCache = [
+        '.',
+        'index.html',
+        'index_bundle.js',
+        'style.css'
+    ];
     
-        caches.open('samresume').then(function(cache) {
-        
-            return cache.addAll([
-            
-                '/',
-                
-                '/index.html',
-                
-                '/style.css',
-                
-                '/index_bundle.js',
-            
-            ]);
-        
-        })
+    var staticCaches = 'my-appshell-cache';
+    var apiCaches = 'my-api-cache';
     
-    );
-
-});
+    self.addEventListener('install', function(event) {
+        console.info('sw intend to intsall and cache file');
+        event.waitUntil(
+            caches.open(staticCaches).then(function(cache) {
+                return cache.addAll(staticFilesToCache);
+            })
+        );
+    });
+    
+    self.addEventListener('active', function(event) {
+        console.info('active new service work');
+        var cacheWhiteList = [staticCaches, apiCaches];
+        event.waitUntil(
+            caches.keys().then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function(cacheName) {
+                        if (cacheWhiteList.indexOf(cacheName) === -1) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                )
+            })
+        )
+    });
+    
+    self.addEventListener('fetch', function(event) {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                if (response) {
+                    console.log('Found' + event.request.url + 'in cache');
+                    return response;
+                }
+                
+            })
+        )
+    })
+})
