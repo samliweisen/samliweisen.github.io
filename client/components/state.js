e962ad0d61303370ead640f1ee5c1d6ed6bb10bf
samliweisen <liweisen007@gmail.com>
2018-03-12 20:18:48 +0000

    update experiences duty

--- client/components/state.js
+++ client/components/state.js
@@ -11,7 +11,9 @@ export const experiences = [
         duty: [
             'Create REST web services and implement restful api with Ajax',
             'Build interactive user interface with angularJS and jQuery',
-            'Train new employee to be familiar with project structure'
+            'Work with designer to create and implement modernized user interface with HTML5/CSS3',
+            'Successfully created web pages and test on BrowserStack and make sure they are compatible with popular web browsers',
+            'Train and help new employees to be familiar with project structure'
         ]
     },
     {
@@ -19,9 +21,12 @@ export const experiences = [
         title: 'Web Developer',
         date: 'Oct 2013 - July 2016',
         duty: [
-            'Build complex interact UIs with ReactJS',
+            'Took the initiative and a self-directed approach to learning to understand and build complex interact UIs with ReactJS',
             'Translate photoshop designs into pixel perfect HTML5/CSS3',
-            'Build maintainable and scalable CSS with BEM architecture pattern'
+            'Build maintainable and scalable CSS with BEM architecture pattern',
+            'Help build platforms that enable clients to create purposeful, goal-oriented websites to support their marketing needs',
+            'Successfully developed and maintained CMS web application for Starwood Hotels and Resorts, Design Hotels and New Castle Hotel brands',
+            'Develop web application using Google Map API and also implemented AutoNavi Map to make sure users in China are able to access map features (note that the majority of Google API’s have been banned in China, fortifying the need for alternate APIs)'
         ]
     }
 ];
