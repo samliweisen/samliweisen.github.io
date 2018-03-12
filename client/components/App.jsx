22eaabe859376b6fb73e92db2f11a64562beb82c
samliweisen <liweisen007@gmail.com>
2018-03-12 19:15:32 +0000

    remove comment

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -22,7 +22,6 @@ class App extends React.Component {
                     <Nav />
                     <Experiences />
                     <Projects />
-                    <Comments />
                 </main>
                 <aside id="aside">
                     <Skills />
