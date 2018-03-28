94348d59e75930057c2a4969c46154eb80d90254
samliweisen <liweisen007@gmail.com>
2018-03-28 19:46:10 +0000

    add todo submit

--- client/pages/Todo.jsx
+++ client/pages/Todo.jsx
@@ -7,8 +7,13 @@ export default class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: []
+            todos: [],
+            newTodo: {
+                name: '',
+                status: 'pending'
+            }
         };
+        this.submitTodo = this.submitTodo.bind(this);
     }
     componentDidMount() {
         this.getTodos();
@@ -23,13 +28,43 @@ export default class Todo extends React.Component {
             }
         });
     }
+    submitTodo() {
+        const topListApi = 'https://samliweisen.herokuapp.com/api/todos';
+        const newTodo = this.state.newTodo;
+        axios.post(topListApi, newTodo).then((res) => {
+            if (res.status == 200) {
+                this.setState({
+                    newTodo: {
+                        name: '',
+                        status: 'pending'
+                    }
+                });
+                this.getTodos();
+            }
+        });
+    }
+    handleKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.submitTodo();
+        }
+    }
+    handleChange(e) {
+        this.setState({
+            newTodo: {
+                name: e.target.value,
+                status: 'pending'
+            }
+        });
+    }
     render() {
         return (
             <div className="container">
                 <h2 className="todos__title">Todo</h2>
                 <div className="todos__container">
+                    <input id="todoName" value={this.state.newTodo.name} onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)} />
                     {this.state.todos.map((todo) => 
                         <div className="todo" key={todo._id}>
+                            <div className="todo__status">{todo.status}</div>
                             <h3 className="todo__title">{todo.name}</h3>
                         </div>
                     )}
