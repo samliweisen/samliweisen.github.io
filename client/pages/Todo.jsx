b6883d74ef04ee7e9f42229ba91100c1573dec64
samliweisen <liweisen007@gmail.com>
2018-03-31 20:03:43 +0000

    add remove todo function

--- client/pages/Todo.jsx
+++ client/pages/Todo.jsx
@@ -12,10 +12,12 @@ export default class Todo extends React.Component {
                 name: '',
                 status: 'pending'
             },
+            loading: false,
             api: {
                 list: 'https://samliweisen.herokuapp.com/api/todos/',
                 add: 'https://samliweisen.herokuapp.com/api/todos/',
-                update: 'https://samliweisen.herokuapp.com/api/todos/'
+                update: 'https://samliweisen.herokuapp.com/api/todos/',
+                remove: 'https://samliweisen.herokuapp.com/api/todos/'
             }
         };
         this.submitTodo = this.submitTodo.bind(this);
@@ -24,9 +26,13 @@ export default class Todo extends React.Component {
         this.getTodos();
     }
     getTodos() {
+        this.setState({
+            loading: true
+        });
         axios.get(this.state.api.list).then((res) => {
             if (res.status == 200) {
                 this.setState({
+                    loading: false,
                     todos: res.data
                 });
             }
@@ -36,13 +42,16 @@ export default class Todo extends React.Component {
         const newTodo = this.state.newTodo;
         axios.post(this.state.api.add, newTodo).then((res) => {
             if (res.status == 200) {
+                const todo = res.data;
+                let todos = this.state.todos;
+                todos.unshift(todo);
                 this.setState({
                     newTodo: {
                         name: '',
                         status: 'pending'
-                    }
+                    },
+                    todos: todos
                 });
-                this.getTodos();
             }
         });
     }
@@ -55,7 +64,7 @@ export default class Todo extends React.Component {
     }
     handleKeyPress(todo, e) {
         if (e.key === 'Enter') {
-            if (typeof todo != 'undefined') {
+            if (todo != 'add') {
                 this.updateTodo(todo);
             } else {
                 this.submitTodo();
@@ -81,21 +90,37 @@ export default class Todo extends React.Component {
         });
         this.setState({todos: newTodos});
     }
+    handleRemove(idx, id) {
+        axios.delete(this.state.api.remove + id).then((res) => {
+            if (res.status == 200) {
+                let todos = this.state.todos;
+                todos.splice(idx, 1);
+                this.setState({
+                    todos: todos
+                });
+            }
+        });
+    }
     getStatus(todo) {
         return 'todo ' + todo.status;
     }
     render() {
+        const todoList = this.state.todos.map((todo, idx) => 
+                        <div className={this.getStatus(todo)} key={todo._id}>
+                            <div className="todo__status">{todo.status}</div>
+                            <input className="todo__title" value={todo.name} onChange={this.handleUpdate.bind(this, idx)} onKeyPress={this.handleKeyPress.bind(this, todo)} />
+                            <div className="todo__remove" onClick={this.handleRemove.bind(this, idx, todo._id)}>Remove</div>
+                        </div>
+                    );
         return (
             <div className="container">
                 <h2 className="todos__title">Todo</h2>
                 <div className="todos__container">
-                    <input id="todoName" value={this.state.newTodo.name} onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)} />
-                    {this.state.todos.map((todo, idx) => 
-                        <div className={this.getStatus(todo)} key={todo._id}>
-                            <div className="todo__status">{todo.status}</div>
-                            <input className="todo__title" value={todo.name} onChange={this.handleUpdate.bind(this, idx)} onKeyPress={this.handleKeyPress.bind(this, todo)} />
-                        </div>
-                    )}
+                    <input id="todoName" value={this.state.newTodo.name} onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this, 'add')} />
+                    {this.state.loading ? 
+                        <div className="loader"></div>
+                        :
+                        todoList}
                 </div>
             </div>
         );
