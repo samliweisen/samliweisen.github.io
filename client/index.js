053638f7c7204eaf02940b1e5e4a0874ef13b8f6
samliweisen <liweisen007@gmail.com>
2018-06-12 20:46:49 +0000

    fix todo list style

--- client/index.js
+++ client/index.js
@@ -1,37 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { createStore,bindActionCreators } from 'redux';
-import { Provider ,connect} from 'react-redux';
 
 import Main from './Main.jsx';
 
-//reducer
-const initialState = {
-    text: 'Hello'
-};
-function myApp(state = initialState, action) {
-    switch (action.type) {
-        case 'CHANGE_TEXT':
-            return {
-                text:state.text=='Hello'?'Stark':'Hello'
-            };
-        case 'BUTTON_CLICK':
-            return {
-                text: 'You just click button'
-            };
-        default:
-            return {
-                text:'Hello'
-            };
-    }
-}
-
-//store
-let store = createStore(myApp);
-
 setTimeout(function() {render(
-    <Provider store={store}>
-        <Main />
-    </Provider>,
+    <Main />,
     document.getElementById('root')
 );}, 3000);
