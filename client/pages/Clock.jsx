fe7807ca6a7f4e54a3aab723a4246a984a1d9f3c
samliweisen <liweisen007@gmail.com>
2018-07-06 20:32:02 +0000

    initial clock page

--- client/pages/Clock.jsx
+++ client/pages/Clock.jsx
@@ -5,35 +5,50 @@ import '../css/clock.css';
 //deadline: new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000)
 
 export default class Clock extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            deadline: new Date(Date.parse(new Date()) + 30 * 60 * 1000)
+            deadline: new Date(Date.parse(new Date()) + 30 * 60 * 1000),
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            milliseconds: 0
         };
     }
     componentDidMount() {
         this.countdown();
-        // var timeinterval = setInterval(this.countdown(), 1000);
+        this.timeinterval = setInterval(() => this.countdown(), 1000);
     }
     countdown() {
-        var t = Date.parse(this.state.deadline) - Date.parse(new Date());
+        var t = this.state.deadline - Date.parse(new Date());
+        var milliseconds = Math.floor((t / 1000));
         var seconds = Math.floor((t / 1000) % 60);
         var minutes = Math.floor((t / 1000 / 60) % 60);
         var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
         var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        return {
+        this.setState({
             'total': t,
             'days': days,
             'hours': hours,
             'minutes': minutes,
-            'seconds': seconds
-        };
+            'seconds': seconds,
+            'milliseconds': milliseconds
+        });
+    }
+    componentWillUnmount() {
+        this.timeinterval.clearInterval();
     }
     render() {
+        const {hours, minutes, seconds} = this.state;
         return (
             <div className="clock">
-                <h2>Time Reminder</h2>
-                
+                <h1>Development in progress</h1>
+                <h2>Remaind me to</h2>
+                <input />
+                <h2>in</h2>
+                <div><input type="number" className="clock__input" /> hours <input type="number" className="clock__input" /> minutes</div>
+                <div>{hours} hours, {minutes} minutes, {seconds} seconds</div>
             </div>
         );
     }
