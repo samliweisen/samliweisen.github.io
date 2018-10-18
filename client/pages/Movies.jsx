ef277559fe756687565f74f5f0c9e0d0ab40e3fd
samliweisen <liweisen007@gmail.com>
2018-10-18 16:34:45 +0000

    build

--- client/pages/Movies.jsx
+++ client/pages/Movies.jsx
@@ -45,13 +45,14 @@ export default class Movies extends React.Component {
                         <div className={status}>{v.current_episode}/{v.episodes}</div>
                         <img className="visual__image" src={v.poster} />
                         <div className="visual__detail">
+                            <p>{v.release_date}</p>
+                            <h3 className="visual__title">{v.title}</h3>
                             <div className="visual__ratings">
                                 <a className="visual__rating" target="_blank" href={'https://movie.douban.com/subject/' + v.douban_id}>
                                     <img className="visual__rating-icon" src="https://img3.doubanio.com/f/talion/2f3c0bc0f35b031d4535fd993ae3936f4e40e6c8/pics/icon/dou32.png" alt="" />
                                     <span className="visual__rating-point">{v.douban_rating}</span>
                                 </a>
                             </div>
-                            <h3 className="visual__title">{v.title}</h3>
                         </div>
                     </div>
                 </div>
